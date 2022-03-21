a80b76ed4f2d99ee37c3c80df9a071d97f8031ba
Rui Lebre <ruilebre@ua.pt>
2022-03-21 11:54:53 +0000

    Added scroll-up button

---
+++ components/scroll-top/ScrollToTheTop.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from 'react';
+import Up from '../icons/Up';
+import styles from './scroll-top.module.scss';
+
+export const ScrollToTheTop = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisibility = () => {
+    if (window.pageYOffset > 80) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility);
+    };
+  }, []);
+
+  return (
+    <div className='fixed bottom-4 right-4'>
+      <button onClick={scrollToTop} className={`${isVisible ? 'opacity-100' : 'opacity-0'} ${styles.button}`}>
+        <Up className='h-6 w-6' aria-hidden='true' />
+      </button>
+    </div>
+  );
+};
+
+export default ScrollToTheTop;
