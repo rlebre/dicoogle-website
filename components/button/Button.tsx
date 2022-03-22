3f3c5cac192b9fcdf252958cdb99c8da4c366f92
Rui Lebre <ruilebre@ua.pt>
2022-03-22 12:27:38 +0000

    Added button target

--- components/button/Button.tsx
+++ components/button/Button.tsx
@@ -6,12 +6,15 @@ interface Props {
   label: string;
   href: string;
   style?: 'button1' | 'button2';
+  target?: string;
 }
 
-const Button = ({ label, href, style }: Props) => {
+const Button = ({ label, href, style, target }: Props) => {
   return (
     <Link href={href}>
-      <a className={`${style === 'button2' ? styles.button2 : styles.button1}`}>{label}</a>
+      <a className={`${style === 'button2' ? styles.button2 : styles.button1}`} target={target}>
+        {label}
+      </a>
     </Link>
   );
 };
